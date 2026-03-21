1203780ba18f00fbd34e62865aea98e681ce9976
Lê Minh Cường <lmc01255289893@gmail.com>
2026-03-21 22:28:35 +0700

    Update solution for precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -3,13 +3,13 @@ def precision_recall_at_k(recommended, relevant, k):
     Compute precision@k and recall@k for a recommendation list.
     """
     # Write code here
-    top_k = []
-    for i in range(0, min(len(recommended), k)):
-        if(recommended[i] in relevant) & (recommended[i] not in top_k):
-            top_k.append(recommended[i])
+    top_k = recommended[:k]
+    
+    relevant_set = set(relevant)
+    hits = sum(1 for item in top_k if item in relevant_set)
             
-    precision = len(top_k) / k
-    recall = len(top_k) / len(relevant)
+    precision = hits / k
+    recall = hits / len(relevant)
     
     return [precision, recall]
 
