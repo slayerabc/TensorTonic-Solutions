8db757dde0555650413e56c9009b245466f6877b
Lê Minh Cường <lmc01255289893@gmail.com>
2026-03-23 12:53:34 +0700

    Update solution for percentiles

--- percentiles/percentiles.py
+++ percentiles/percentiles.py
@@ -1,14 +1,5 @@
 import numpy as np
-
-def compute(i, x, n):
-    if (i == 0.0) | (i == float(n-1)): 
-        return x[int(i)]
-
-    IQR = x[int(i)+1] - x[int(i)] 
     
-    return np.where(i == float(int(i)), x[int(i)], x[int(i)] + (i - int(i)) * IQR)
-    
-
 def percentiles(x, q):
     """
     Compute percentiles using linear interpolation.
@@ -16,8 +7,15 @@ def percentiles(x, q):
     # Write code here
     x = np.asarray(x, dtype = float)
     x = np.sort(x)
-    n = x.size 
+    n = x.size
     q = (np.asarray(q, dtype = float)/100) * (n-1)
 
+    low = q.astype(int)
+    high = np.clip(low+1, 0, n-1)
 
-    return np.array([compute(i, x, n) for i in q])
+    IQR = q - low
+
+    return (1 - IQR) * x[low] + IQR * x[high]
+    
+    
+    
